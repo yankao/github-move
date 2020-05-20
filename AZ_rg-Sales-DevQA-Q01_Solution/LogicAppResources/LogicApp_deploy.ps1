1f9f32c956e974200744575c4a3a3c7ee9ecc0d6
Eddie Kao <eddie.kao@capgemini.com>
2020-05-20 13:49:09 -0400

    Logic App Deploy 5/20 13:49

--- AZ_rg-Sales-DevQA-Q01_Solution/LogicAppResources/LogicApp_deploy.ps1
+++ AZ_rg-Sales-DevQA-Q01_Solution/LogicAppResources/LogicApp_deploy.ps1
@@ -1,6 +1,10 @@
 #
 # LogicApp_deploy.ps1
 #
-Test-AzResourceGroupDeployment -TemplateParameterFile ".\LogicApp.parameters.json" -TemplateFile ".\LogicApp-Copy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-2]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
+Test-AzResourceGroupDeployment -TemplateFile ".\appLA_S3F\appLA-S3F-REALSfdc2SdfDeleteAccountContact-ARM.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-2]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
 
-New-AzResourceGroupDeployment -TemplateParameterFile ".\LogicApp.parameters.json" -TemplateFile ".\LogicApp-Copy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-2]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
+New-AzResourceGroupDeployment -TemplateFile ".\appLA_S3F\appLA-S3F-REALSfdc2SdfDeleteAccountContact-ARM.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-2]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
+
+Test-AzResourceGroupDeployment -TemplateFile ".\appLA_S3F\appLA-S3F-REALSfdc2SdfPostAccountProfile-ARM.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-2]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
+
+New-AzResourceGroupDeployment -TemplateFile ".\appLA_S3F\appLA-S3F-REALSfdc2SdfPostAccountProfile-ARM.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-2]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
