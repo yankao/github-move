d67017f0b2e115418c4eeddf5ff795a927bea4fe
Eddie Kao <eddie.kao@capgemini.com>
2020-06-03 19:50:54 +0000

    Deleted DeployResources

--- AZ_rg-Sales-DevQA-Q01_Solution/DeployResources/ResourceGroup/resource_group_tag.ps1
+++
@@ -1,5 +0,0 @@
-#
-# resource_group_tag.ps1
-#
-set-AzResourceGroup -Name  ((dir).DirectoryName.Split("\")[-3]).TrimStart("AZ_").Replace("_Solution","") -Tag @{BA="ENT"; Landscape="PROD"; Solution="Network - Prod(CI1340052)"; OpCo="ALCS"}
-
