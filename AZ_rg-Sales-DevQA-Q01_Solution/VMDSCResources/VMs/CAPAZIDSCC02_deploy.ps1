c9fc837fac2cfdf2a3e7d32bec9e2f1de20f57fb
Eddie Kao <eddie.kao@capgemini.com>
2020-07-20 08:05:25 -0400

    Update NIC 202007200805

--- AZ_rg-Sales-DevQA-Q01_Solution/VMDSCResources/VMs/CAPAZIDSCC02_deploy.ps1
+++ AZ_rg-Sales-DevQA-Q01_Solution/VMDSCResources/VMs/CAPAZIDSCC02_deploy.ps1
@@ -1,9 +1,9 @@
 #
 # CAPAZIDSCC02_deploy.ps1
 #
-Test-AzResourceGroupDeployment -TemplateParameterFile "..\AzureDeployParameters\CAPAZIDSCC02.parameters.json" -TemplateFile "..\AzureDeployTemplates\virtual_machine_NICs.v2.2.deploy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-3]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
+Test-AzResourceGroupDeployment -TemplateParameterFile "..\AzureDeployParameters\CAPAZIDSCC02.parameters.json" -TemplateFile "..\AzureDeployTemplates\virtual_machine_NICs.v2.4.deploy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-3]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
 
-New-AzResourceGroupDeployment -TemplateParameterFile "..\AzureDeployParameters\CAPAZIDSCC02.parameters.json" -TemplateFile "..\AzureDeployTemplates\virtual_machine_NICs.v2.2.deploy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-3]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
+New-AzResourceGroupDeployment -TemplateParameterFile "..\AzureDeployParameters\CAPAZIDSCC02.parameters.json" -TemplateFile "..\AzureDeployTemplates\virtual_machine_NICs.v2.4.deploy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-3]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
 
 Test-AzResourceGroupDeployment -TemplateParameterFile "..\AzureDeployParameters\CAPAZIDSCC02.parameters.json" -TemplateFile "..\AzureDeployTemplates\virtual_machine.v2.2.deploy.json" -ResourceGroupName ((dir).DirectoryName.Split("\")[-3]).TrimStart("AZ_").Replace("_Solution","") -Verbose -ErrorAction Stop
 
